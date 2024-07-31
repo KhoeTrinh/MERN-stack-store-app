b9445ebd6e258fb2fa507d7e3205e50db6f287f4
KhoeTrinh <kelvintrinh09@gmail.com>
2024-07-31 17:04:30 +0700

    bruh i miss the next() and it took me 1 hour :D

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,7 +13,11 @@ import {
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
-router.route('/').post(createUser).get(authenticate, authorizeAdmin, getAllUsers)
+router
+    .route('/')
+    .post(createUser)
+    .get(authenticate, authorizeAdmin, getAllUsers)
+    
 router.post('/auth', loginUser)
 router.post('/logout', logoutCurrentUser)
 
