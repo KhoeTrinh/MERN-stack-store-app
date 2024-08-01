b452e87a97b45b812afb127f1bcb62324d1218c1
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-01 15:17:38 +0700

    ca

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -4,10 +4,60 @@ import asyncHandler from '../middlewares/asyncHandler.js'
 const createCategory = asyncHandler(async (req, res) => {
     try {
         const { name } = req.body
+
+        if(!name) {
+            return res.json({error: "Name is required bro 🤦‍♂️"})
+        }
+
+        const existingCategory = await Category.findOne({ name })
+
+        if(existingCategory) {
+            return res.json({error: "Category already exists man ☠️"})
+        }
+
+        const category = await new Category({ name }).save()
+        res.json(category)
+
     } catch (error) {
         console.log(error)
         return res.status(400).json(error)
     }
 })
 
-export { createCategory }
+const updateCategory = asyncHandler(async (req, res) => {
+    try {
+        const { name } = req.body
+        const { categoryId } = req.params
+
+        const category = await Category.findOne({_id: categoryId})
+
+        if(!category) {
+           return res.status(404).json({error: "Category not found"})
+        }
+
+        category.name = name
+        
+        const updateCategory = await category.save()
+        res.json(updateCategory)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+})
+
+const deleteCategory = asyncHandler(async (req, res) => {
+    try {
+        const removed = await Category.findByIdAndDelete(req.params.categoryId)        
+        res.json(removed)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Internal server error'})
+    }
+})
+
+export { 
+    createCategory, 
+    updateCategory,
+    deleteCategory
+}
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import { createCategory } from "../controllers/categoryController.js";
+import { 
+    createCategory, 
+    updateCategory,
+    deleteCategory
+} from "../controllers/categoryController.js";
 import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js'
  
-router.route('/').post(createCategory)
+router.route('/').post(authenticate, authorizeAdmin, createCategory)
+router.route('/:categoryId').put(authenticate, authorizeAdmin, updateCategory)
+router.route('/:categoryId').delete(authenticate, authorizeAdmin, deleteCategory)
 
 export default router
