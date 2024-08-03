98c8ea57d59f84eae93b8a72922f199aa7ef36f7
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-03 12:48:30 +0700

    0987654321

--- frontend/src/pages/Admin/ProductList.jsx
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -1,9 +1,150 @@
-import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {
+    useCreateProductMutation,
+    useUploadProductImageMutation,
+} from '../../redux/api/productApiSlice'
+import { useFetchCategoriesQuery } from '../../redux/api/categoryApiSlice'
+import { toast } from 'react-toastify'
 
 const ProductList = () => {
-  return (
-    <div>ProductList</div>
-  )
-}
+    const [ image, setImage ] = useState('')
+    const [ name, setName ] = useState('')
+    const [ description, setDescription ] = useState('')
+    const [ price, setPrice ] = useState('')
+    const [ category, setCategory ] = useState('')
+    const [ quantity, setQuantity ] = useState('')
+    const [ brand, setBrand ] = useState('')
+    const [ stock, setStock ] = useState(0)
+    const [ imageUrl, setImageUrl ] = useState(null)
+
+    const navigate = useNavigate()
+
+    const [ uploadProductImage ] = useUploadProductImageMutation()
+    const [ createProduct ] = useCreateProductMutation() 
+    const { data: categories } = useFetchCategoriesQuery()
+
+
+return (
+    <div className="container xl:mx-[9rem] sm:mx-[0]">
+      <div className="flex flex-col md:flex-row">
+        <div className="md:w-3/4 p-3">
+          <div className="h-12">Create Product</div>
+
+          {imageUrl && (
+            <div className="text-center">
+              <img
+                src={imageUrl}
+                alt="product"
+                className="block mx-auto max-h-[200px]"
+              />
+            </div>
+          )}
+
+          <div className="mb-3">
+            <label className="border text-white px-4 block w-full text-center rounded-lg cursor-pointer font-bold py-11">
+              {image ? image.name : "Upload Image"}
+
+              <input
+                type="file"
+                name="image"
+                accept="image/*"
+                // onChange={uploadFileHandler}
+                className={!image ? "hidden" : "text-white"}
+              />
+            </label>
+          </div>
+
+          <div className="p-3">
+            <div className="flex flex-wrap">
+              <div className="one">
+                <label htmlFor="name">Name</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="two ml-10 ">
+                <label htmlFor="name block">Price</label> <br />
+                <input
+                  type="number"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="one">
+                <label htmlFor="name block">Quantity</label> <br />
+                <input
+                  type="number"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg"
+                  value={quantity}
+                  onChange={(e) => setQuantity(e.target.value)}
+                />
+              </div>
+              <div className="two ml-10 ">
+                <label htmlFor="name block">Brand</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg"
+                  value={brand}
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <label htmlFor="" className="my-5">
+              Description
+            </label>
+            <textarea
+              type="text"
+              className="p-2 mb-3 border rounded-lg w-[92%]"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></textarea>
+
+            <div className="flex flex-wrap">
+              <div>
+                <label htmlFor="name block">Count In Stock</label> <br />
+                <input
+                  type="text"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg"
+                  value={stock}
+                  onChange={(e) => setStock(e.target.value)}
+                />
+              </div>
+
+              <div className='ml-10'>
+                <label htmlFor="">Category</label> <br />
+                <select
+                  placeholder="Choose Category"
+                  className="p-4 mb-3 w-[25rem] border rounded-lg"
+                  onChange={(e) => setCategory(e.target.value)}
+                >
+                  {categories?.map((c) => (
+                    <option key={c._id} value={c._id}>
+                      {c.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+
+            <button
+            //   onClick={handleSubmit}
+              className="py-4 px-10 mt-5 rounded-lg text-lg font-bold bg-pink-600"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProductList
