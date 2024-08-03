122d37b80cf8ac392aa2e26ca3a86786ab6887c9
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-03 11:38:12 +0700

    1234567890

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -167,6 +167,26 @@ const addProductReview = asyncHandler(async (req, res) => {
     }
   });
 
+const fetchTopProducts = asyncHandler(async (req, res) => {
+    try {
+        const products = await Product.find({}).sort({ rating: -1 }).limit(4)
+        res.json(products);
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error.message)
+    }
+})
+
+const fetchNewProducts = asyncHandler(async (req, res) => {
+    try {
+        const products = await Product.find().sort({_id: -1}).limit(5)
+        res.json(products)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json(error.message)
+    }
+})
+
 export { 
     addProduct, 
     updateProduct, 
@@ -174,5 +194,7 @@ export {
     fetchProduct,
     fetchProductById,
     fetchAllProducts,
-    addProductReview
+    addProductReview,
+    fetchTopProducts,
+    fetchNewProducts
 } 
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,9 +11,14 @@ import {
     fetchProduct,
     fetchProductById,
     fetchAllProducts,
-    addProductReview
+    addProductReview,
+    fetchTopProducts,
+    fetchNewProducts
 } from '../controllers/productController.js'
-import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import { 
+    authenticate, 
+    authorizeAdmin 
+} from "../middlewares/authMiddleware.js";
 import checkId from "../middlewares/checkId.js"
 
 router
@@ -21,8 +26,16 @@ router
     .get(fetchProduct)
     .post(authenticate, authorizeAdmin, formidable(), addProduct)
 
-router.route('/allproducts').get(fetchAllProducts)
-router.route('/:id/reviews').post(authenticate, authorizeAdmin, addProductReview)
+router
+    .route('/allproducts')
+    .get(fetchAllProducts)
+
+router
+    .route('/:id/reviews')
+    .post(authenticate, authorizeAdmin, checkId, addProductReview)
+
+router.get('/top', fetchTopProducts)
+router.get('/new', fetchNewProducts)
     
 router
     .route('/:id')
